199d553e64b22f02343031fd953d6893dffeb61d
Nikin Tharan <nikin.tharan@gmail.com>
2016-01-17 04:21:01 -0500

    Changes in early morning 2

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -2,10 +2,14 @@
 
 //Detect click and amalgamate newsletter in local storage
 chrome.extension.onRequest.addListener(
-  function(connectionInfo) {
+  function() {
     // Push to local storage
-    var article = helpers.buildArticle();
-	localStorage.setItem('Article', JSON.stringify(article));
+   // var article = helpers.buildArticle();
+	//localStorage.setItem('Article', JSON.stringify(article));
+	createGmail();
+	console.log("Test whatever");
 });
 
-
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('createnewsletterButton').addEventListener('click', createGmail);
+});
