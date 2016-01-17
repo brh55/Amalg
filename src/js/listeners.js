0b0400a76b0ab101288a5d653ee7d70c65aeebb1
Brandon Him <brh55@drexel.edu>
2016-01-17 09:54:11 -0500

    Revert "Changed <main> in index.html to <div class="content">"
    This reverts commit f24c230935e0ed9b82f944a912ca71992de82ca1.

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -7,5 +7,6 @@ window.onload = function() {
 	// Push to local storage
     var article = helpers.buildArticle();
 	localStorage.setItem('Article', JSON.stringify(article));
-	$( ".content" ).append(article);
+	
+	
 }
