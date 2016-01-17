31f72ecab94ef69e3814430e6dd9d7757d2bf4c1
Brandon Him <brh55@drexel.edu>
2016-01-17 02:54:59 -0500

    Update to get it to work

--- src/js/settings/settings.js
+++ src/js/settings/settings.js
@@ -8,10 +8,6 @@ var save_options = function () {
       // Update status to let user know options were saved.
       var status = document.getElementById('status');
       status.textContent = 'Options saved.';
-
-      setTimeout(function() {
-         status.textContent = '';
-      }, 750);
    });
 };
 
